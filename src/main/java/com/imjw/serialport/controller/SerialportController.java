cc1a31ea600c6418475a6082a033976bb84d7105
wujianwei <wujianweia@itiaoling.com>
2018-10-11 10:04:01 +0800

    [optimize]修改包目录

--- src/main/java/com/imjw/serialport/controller/SerialportController.java
+++ src/main/java/com/imjw/serialport/controller/SerialportController.java
@@ -1,4 +1,4 @@
-package com.imjw.controller;
+package com.imjw.serialport.controller;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.imjw.service.SerialportService;
+import com.imjw.serialport.service.SerialportService;
 
 @RestController
 @RequestMapping("/serialport")
